06e072ec4a1c3e53749fcbfdd5e6c93c7bdb811c
Xantibody <zeku.bushinryu38@gmail.com>
2024-10-06 20:12:03 +0900

    ctr + / でcommentoutに対応した

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,8 +1,15 @@
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
-    'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    }
+  'numToStr/Comment.nvim',
+  keys = {
+    {
+      '<C-/>',
+      "<cmd>lua require('Comment.api').toggle.linewise.current()<CR><Esc>A",
+      mode = { 'n', 'i' },
+      desc = 'comment out line',
+    },
+  },
+  opts = {
+    -- add any options here
+  }
 }
-
