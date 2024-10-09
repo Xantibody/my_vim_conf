5c861c938983312020fa022372988eb501f08b04
Xantibody <zeku.bushinryu38@gmail.com>
2024-10-10 00:00:23 +0900

    masonのLSPを記述でインストールに変更し、コード管理

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,30 +1,37 @@
 return {
   "williamboman/mason.nvim",
   dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason-lspconfig.nvim",
   },
-  config = function ()
+  config = function()
     local mason = require("mason")
 
     local mason_lspconfig = require("mason-lspconfig")
- 
+
     mason.setup({
       ui = {
         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
         }
-    }
+      }
     })
 
     -- Not required if installing with Mason UI
-  -- mason_lspconfig.setup({
-  --     ensure_installed = {
-  --       "lua_ls",
-  --     },
-  --
-  --     Automatic_installed = true,
-  --   })
- end
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+        "jdtls",
+        "rust_analyzer",
+        "marksman",
+        "yamlls",
+        "pylsp",
+        "ts_ls",
+
+      },
+
+      Automatic_installed = true,
+    })
+  end
 }
